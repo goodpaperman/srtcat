9673f07cc389962beb107decf3a4f788510c90d9
yunhai <haihai107@126.com>
2023-01-17 23:36:32 +0800

    can run now

--- srtcat.sh
+++ srtcat.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
 
+CONCATE_RESULT=";"
+
 function usage()
 {
-    echo "Usage: srtcat [-t seprate] file1 file2 ..."
+    echo "Usage: srtcat [-t timespan] file1 file2 ..."
 }
 
 # @brief: add hour:min:sec,msec to another 
@@ -49,102 +51,108 @@ function time_add()
     fi
 
     h3=$((h1+h2+h3))
+    # can exceed 99 now
+    # if [ ${h3} -gt 99 ]; then 
+    #     echo "hour (${h3}) exceed 99!" 1>&2
+    # fi
+
     # return "curr_hour=${h3};curr_min=${m3};curr_sec=${s3};curr_msec=${ms3}"
+    echo "time add: ${h1}:${m1}:${s1},${ms1} + ${h2}:${m2}:${s2},${ms2} = ${h3}:${m3}:${s3},${ms3}" 1>&2
     echo "${h3}:${m3}:${s3},${ms3}"
 }
 
 # @brief: srt single file concating, dumping to stdin
-# @param: start hour
-# @param: start min
-# @param: start sec
-# @param: start msec
-# @param: time separate between file (in sec)
-# @param: start ordinal, 0 means first file
+# @param: hour base
+# @param: min base
+# @param: sec base
+# @param: msec base
+# @param: time span between file (in msec)
+# @param: base ordinal, 0 means first file
 # @param: srt file
 # @return: new time & new ordinal
 function concate_file()
 {
-    local start_hour=$1
-    local start_min=$2
-    local start_sec=$3
-    local start_msec=$4
-    local separate=$5
-    local start_ord=$6
+    local hour_base=$1
+    local min_base=$2
+    local sec_base=$3
+    local msec_base=$4
+    local time_span=$5
+    local ord_base=$6
     local file=$7
     local curr_ord=0
-    local curr_hour=0
-    local curr_min=0
-    local curr_sec=0
-    local curr_msec=0
     local curr_srt=""
+    # start time
+    local hour1=0
+    local min1=0
+    local sec1=0
+    local msec1=0
+    # end time
+    local hour2=0
+    local min2=0
+    local sec2=0
+    local msec2=0
+
     local line=""
     local n=0 
-    local m=0
     local val=""
     local tmp=""
 
+    echo "base time: ${hour_base}:${min_base}:${sec_base},${msec_base}; base ord: ${ord_base}" 1>&2
     while read line; do
-        m=$((n%4))
-        # case $m in
-        #    0)
-        if [ $m -eq 0 ]; then
+        line=$(echo "${line}" | tr -d '\r')
+        # echo "line=${line}" 1>&2
+        case $((n%4)) in
+            0)
                 # ordinal
-                curr_ord=$((${line}+${start_ord}))
+                curr_ord=$((line+ord_base))
                 echo "${curr_ord}"
-                #;;
-        elif [ $m -eq 1 ]; then 
-        #    1)
+                ;;
+            1)
                 # time range
-                #    start time
-                curr_hour=$(echo "${line}" | cut -b 1-2)
-                curr_min=$(echo "${line}" | cut -b 4-5)
-                curr_sec=$(echo "${line}" | cut -b 7-8)
-                curr_msec=$(echo "${line}" | cut -b 10-12)
-                val=$(time_add "${curr_hour}" "${curr_min}" "${curr_sec}" "${curr_msec}" "${start_hour}" "${start_min}" "${start_sec}" "${start_msec}")
-                # eval "${val}"
-                # tmp="${curr_hour}:${curr_min}:${curr_sec},${curr_msec} ---> "
-                tmp="${val}"
-                #    end time
-                curr_hour=$(echo "${line}" | cut -b 1-2)
-                curr_min=$(echo "${line}" | cut -b 4-5)
-                curr_sec=$(echo "${line}" | cut -b 7-8)
-                curr_msec=$(echo "${line}" | cut -b 10-12)
-                val=$(time_add "${curr_hour}" "${curr_min}" "${curr_sec}" "${curr_msec}" "${start_hour}" "${start_min}" "${start_sec}" "${start_msec}")
-                # eval "${val}"
-                # tmp="${tmp}${curr_hour}:${curr_min}:${curr_sec},${curr_msec}"
-                tmp="${tmp} ---> ${val}"
-                echo "${tmp}"
-                #;;
-        #     2)
-        elif [ $m -eq 2 ]; then 
+                # 00:01:02,003 --> 04:05:06,007 after break:
+                #  1  2  3  4  56   7  8  9  10 
+                # note add $X/1 to remove leading 0s
+                val=$(echo "${line}" | awk -F':|,|-|>' '{print "hour1="$1/1";min1="$2/1";sec1="$3/1";msec1="$4/1";hour2="$7/1";min2="$8/1";sec2="$9/1";msec2="$10/1";"}')
+                eval "${val}"
+                if [ ${hour_base} -eq 0 -a ${min_base} -eq 0 -a ${sec_base} -eq 0 -a ${msec_base} -eq 0 ]; then 
+                    echo "${line}"
+                else 
+                    tmp=$(time_add "${hour1}" "${min1}" "${sec1}" "${msec1}" "${hour_base}" "${min_base}" "${sec_base}" "${msec_base}")
+                    val=$(time_add "${hour2}" "${min2}" "${sec2}" "${msec2}" "${hour_base}" "${min_base}" "${sec_base}" "${msec_base}")
+                    echo "${tmp} --> ${val}"
+                fi 
+                ;;
+            2)
                 # srt
                 echo "${line}"
-                #;;
-        elif [ $m -eq 3 ]; then 
-        #     3)
+                ;;
+            3)
                 # empty line
-                if [ -n ${line} ]; then 
-                    echo "should be empty line, fatal error!"
+                if [ -n "${line}" ]; then 
+                    echo "should be empty line, fatal error! ${line}" 1>&2
                     exit 1
                 fi
-                #;;
-        else
-        #    *)
-                echo "invalid result: $m, fatal error!"
+                echo ""
+                ;;
+            *)
+                echo "invalid result: $m, fatal error!" 1>&2
                 exit 1
-                #;;
-        fi
-        #esac
+                ;;
+        esac
         n=$((n+1))
     done < ${file}
 
-    # add separate seconds into time..
-    val=$(time_add "${curr_hour}" "${curr_min}" "${curr_sec}" "${curr_msec}" "0" "0" "${separate}" "0")
-    curr_hour=$(echo "${val}" | cut -b 1-2)
-    curr_min=$(echo "${val}" | cut -b 4-5)
-    curr_sec=$(echo "${val}" | cut -b 7-8)
-    curr_msec=$(echo "${val}" | cut -b 10-12)
-    echo "start_hour=${current_hour};start_min=${current_min};start_sec=${curr_sec};start_msec=${curr_msec};start_ord=${curr_ord};"
+    # first parse the last end time after added with base time
+    tmp=$(echo "${val}" | awk -F':|,' '{print "hour2="$1/1";min2="$2/1";sec2="$3/1";msec2="$4/1";"}')
+    eval "${tmp}"
+    # add timespan milli-seconds into end time..
+    val=$(time_add "${hour2}" "${min2}" "${sec2}" "${msec2}" "0" "0" "0" "${time_span}")
+    # 00:01:02,003 after break:
+    #  1  2  3  4  
+    tmp=$(echo "${val}" | awk -F':|,' '{print "hour1="$1/1";min1="$2/1";sec1="$3/1";msec1="$4/1";"}')
+    eval "${tmp}"
+
+    CONCATE_RESULT="start_hour=${hour1};start_min=${min1};start_sec=${sec1};start_msec=${msec1};start_ord=${curr_ord};"
 }
 
 # @brief: srt main
@@ -153,14 +161,14 @@ function concate_file()
 # @retval: 1 - error
 function main()
 {
-    local separate=1  # unit in sec
+    local timespan=1000  # unit in msec
     while getopts "t:" opt
     do
         case ${opt} in
             t)
-                separate=${OPTARG}
+                timespan=${OPTARG}
                 # debug log goes to stderr
-                echo "time separate between file is: ${separate} s" 1>&2
+                echo "timespan between file is: ${timespan} ms" 1>&2
                 ;;
             ?)
                 echo "unknown option at ${OPTIND}"
@@ -172,6 +180,11 @@ function main()
 
     # after shift, file1/file2.. can access by $1/$2 directly..
     shift $(($OPTIND-1))
+    if [ $# -lt 1 ]; then 
+        usage
+        exit 1
+    fi
+
     local start_hour=0
     local start_min=0
     local start_sec=0
@@ -182,9 +195,10 @@ function main()
     do
         # debug log goes to stderr
         echo "handle file ${arg}" 1>&2
-        res=$(concate_file "${start_hour}" "${start_min}" "${start_sec}" "${start_msec}" "${separate}" "${start_ord}" "${arg}")
+        concate_file "${start_hour}" "${start_min}" "${start_sec}" "${start_msec}" "${timespan}" "${start_ord}" "${arg}"
         # return: start_sec=xxx;start_msec=xxx;start_ord=xxx
-        eval "${res}"
+        # echo "${CONCATE_RESULT}" 1>&2
+        eval "${CONCATE_RESULT}"
         # debug log goes to stderr
         echo "last time: ${start_hour}:${start_min}:${start_sec},${start_msec}; last ord: ${start_ord}" 1>&2
     done
